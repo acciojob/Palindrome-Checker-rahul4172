23f3a2efc8b608f3d4707183707f617f5e997340
rahul4172 <96787274+rahul4172@users.noreply.github.com>
2022-10-21 10:01:22 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,11 +1,12 @@
 // complete the given function
 
 function palindrome(str){
-  let j = str.length -1;
+  let str1 = str.toUpperCase();
+  let j = str1.length -1;
     for( let i = 0 ; i < j/2 ;i++)
     {
-      let x = str[i] ;
-      let y = str[j-i];
+      let x = str1[i] ;
+      let y = str1[j-i];
       if( x != y)
       {
     
