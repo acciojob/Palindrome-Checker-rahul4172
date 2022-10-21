1d5d7762b933e26ac9df11f5cefd4f6a4957c5f0
rahul4172 <96787274+rahul4172@users.noreply.github.com>
2022-10-21 09:55:09 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,6 +1,26 @@
 // complete the given function
 
 function palindrome(str){
-
+  let ans = true;
+  let j = str.length -1;
+    for( let i = 0 ; i < j/2 ;i++)
+    {
+      let x = str[i] ;
+      let y = str[j-i];
+      if( x != y)
+      {
+    
+        ans = false;
+      }
+    }
+    if( ans == true )
+    {
+      console.log("passed string is palindrome ");
+    }
+    else
+    {
+      console.log("passed string not a palindrome");
+    }
 }
+
 module.exports = palindrome
